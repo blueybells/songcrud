b4097c1ff8677be48518477a80a91c2a2897bf3e
blueybells <116429112+blueybells@users.noreply.github.com>
2022-10-29 05:53:21 +0100

    Registered the models on the admin Site

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import Artiste, Song, Lyric
 
 # Register your models here.
+
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age")
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("title", "singer", "date_released")
+    ordering = ["-date_released"]
+
+@admin.register(Lyric)
+class LyricAdmin(admin.ModelAdmin):
+  list_display = ("song_for", "song_id")
